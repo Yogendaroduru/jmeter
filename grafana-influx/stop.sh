669dfda1439f8d4027f8b30a87a036c915d4125f
Yogendaroduru <123927531+Yogendaroduru@users.noreply.github.com>
2023-02-07 22:47:10 +0530

    Delete stop.sh

--- grafana-influx/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CONFIGPATH=`pwd`/config/
-#Stop Influxdb and Grafana containers
-microk8s kubectl delete -f $CONFIGPATH/influxdb.yaml
-microk8s kubectl delete -f $CONFIGPATH/grafana.yaml
